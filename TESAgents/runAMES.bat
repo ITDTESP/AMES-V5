32a7769ca9ba56dde727248f44d452e682a7936f
Girraj Raigar <113358861+RaigarGirraj@users.noreply.github.com>
2024-08-14 13:39:17 +0530

    configuration of yamlWriter.py included

--- TESAgents/runAMES.bat
+++ TESAgents/runAMES.bat
@@ -1,21 +1,21 @@
-set TSDir=C:\ITDTESPlatform
-rem set TSDir=C:\Users\swathi\Dropbox\ITDTESPlatform
+for /f "tokens=5" %%a in ('netstat -aon ^| find ":5570" ') do taskkill /f /pid %%a
 
-set AmesVersion=AMES-V5.1
-set AMESDir=%TSDir%\%AmesVersion%
-set TESDir=%AMESDir%\TESAgents
+set TESDir=%cd%
+set AMESDir=%TESDir%/../
+
+for /d %%i in (../../AMES-V5*) do set AmesVersion=%%i
+
 set fncsLibDir=%AMESDir%\fncsDependencies
-
 set ForecastDir=%TESDir%\forecast
 set YAMLFilesDir=%TESDir%\yamlFiles
 set OutputFilesDir=%TESDir%\outputFiles
 set LogFilesDir=%OutputFilesDir%\logFiles
 
-
-
 md %OutputFilesDir% 2> nul
 md %LogFilesDir% 2> nul
 
+SET LookForFile="%OutputFilesDir%/CaseName.txt"
+IF EXIST %LookForFile% del "%OutputFilesDir%\CaseName.txt"
 
 cd %AMESDir%
 
@@ -25,36 +25,45 @@ set FNCS_TRACE=no
 set FNCS_LOG_LEVEL=DEBUG2
 set FNCS_CONFIG_FILE=%YAMLFilesDir%/ames.yaml
 start /b cmd /c java -jar -Djava.library.path=%fncsLibDir% "%AMESDir%/dist/%AmesVersion%.jar"^ > %LogFilesDir%/ames.log 2^>^&1
-
-
-
-
-
 cd %TESDir%
 
-set "file_path=%OutputFilesDir%/newfile.txt"
+@ECHO OFF
+SET LookForFile="%OutputFilesDir%/CaseName.txt"
+
+:CheckForFile
+IF EXIST %LookForFile% GOTO FoundIt
+
+GOTO CheckForFile
+
+:FoundIt
+rem ECHO Found: %LookForFile%
+
+set "file_path=%OutputFilesDir%/CaseName.txt"
 
 set /p content=<%file_path%
-echo Content: %content%
+rem echo Content: %content%
 
 set Param=MaxDay
-
 for /f "tokens=1,2" %%a in (%AMESDir%/DATA/%content%.dat) do ( if %%a==%Param% set MaxDay=%%b )
-rem echo The value of the MaximumDays is %MaxDay% > checkBatch.txt
-for /f "tokens=5" %%a in ('netstat -aon ^| find ":5570" ') do taskkill /f /pid %%a
 
+set Param=NLSE
+for /f "tokens=1,2" %%a in (%AMESDir%/DATA/%content%.dat) do ( if %%a==%Param% set NLSE=%%b )
+
+set Param=NumberOfBuses
+for /f "tokens=1,2" %%a in (%AMESDir%/DATA/%content%.dat) do ( if %%a==%Param% set NBus=%%b )
 
 set Param=RTOPDur
-
 for /f "tokens=1,2" %%a in (%AMESDir%/DATA/%content%.dat) do ( if %%a==%Param% set RTOPDur=%%b )
 
+
+if [%1]==[] (set "ITD=0") else (set ITD=%1)
+if [%2]==[] (set "TxBus=1") else (set TxBus=%2)
+
 set "NHour=4"
-set "deltaT=300"
 set /a "tmax=%MaxDay%*86400+%NHour%*3600"
 set /a "NoOfProcesses=3"
 
-
-
+start /b cmd /c python yamlWriter.py %NLSE% %NBus% %ITD% %TxBus%
 
 set FNCS_CONFIG_FILE=%YAMLFilesDir%/NetLoadForecastDAM.yaml
 set FNCS_LOG_LEVEL=
