93ecc8493cde0b5d22fc51862380db55d500c885
Girraj Raigar <113358861+RaigarGirraj@users.noreply.github.com>
2024-10-10 17:25:38 +0530

    Add files via upload

--- TESAgents/yamlWriter.py
+++ TESAgents/yamlWriter.py
@@ -1,16 +1,47 @@
 import sys
+import os
 import yaml
 
 YAMLPath = "./yamlFiles/"
 TSAgent = 'ames' 
 
+input_file_name = sys.argv[1] + ".dat"
+current_directory = os.path.dirname(os.path.abspath(__file__))
+data_directory = os.path.abspath(os.path.join(current_directory, '..', 'data'))
+input_data_file = os.path.join(data_directory, input_file_name)
+print(f"Data directory: +{data_directory}")
 
-NLSE = int(sys.argv[1])
-NTxBus = int(sys.argv[2])
-ITD = int(sys.argv[3])
-TxBus = int(sys.argv[4])
+NLSE = None
+NTxBus = None
+
+if len(sys.argv) == 4:
+    ITD = int(sys.argv[2])
+    TxBus = int(sys.argv[3])
+if len(sys.argv) == 3:
+    ITD = int(sys.argv[2])
+    TxBus = 1
+if len(sys.argv) == 2:
+    ITD = 0
+    TxBus = 1
+
+try:
+    with open(input_data_file, 'r') as file:
+        for line in file:
+            parts = line.strip().split()
+            if len(parts) >= 2:
+                if parts[0] == 'NLSE':
+                    NLSE = int(parts[1])  
+                elif parts[0] == 'NumberOfBuses':
+                    NTxBus = int(parts[1]) 
+except FileNotFoundError:
+    print(f"Error: The file {input_data_file} was not found.")
+
+# NLSE = int(sys.argv[1])
+# NTxBus = int(sys.argv[2])
+# ITD = int(sys.argv[3])
+# TxBus = int(sys.argv[4])
 	
-#print("NLSE:", NLSE,"NTxBus:", NTxBus, "ITD:", ITD, "TxBus:", TxBus) 
+print("NLSE:", NLSE,"NTxBus:", NTxBus, "ITD:", ITD, "TxBus:", TxBus) 
 NHours = 24
 
 auction_datayaml = {}
@@ -33,3 +64,5 @@ if ITD == 1:
 
 with open( YAMLPath + TSAgent + '.yaml', 'w') as outfile: 
     yaml.dump(auction_datayaml, outfile, default_flow_style=False)
+
+print("The yamlWriteer.py has executed successfully.")
